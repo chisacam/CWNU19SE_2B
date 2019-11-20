1d54e81fa6758b70f549d383721737b3280e78d9
SiGae <SiGae@protonmail.com>
2019-11-20 17:55:58 +0900

    edit mainpage and weather (지도 height % 아직안됨)

--- 2B/app.py
+++ 2B/app.py
@@ -5,8 +5,13 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def mainPage():
+def main_page():
     return render_template("index.html")
+
+
+@app.route('/weather')
+def weather_page():
+    return render_template("/weather.html")
 
 
 if __name__ == '__main__':
