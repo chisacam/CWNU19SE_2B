74c5335caf3bac120d176dc7dff5968cccb12aa4
SiGae <SiGae@protonmail.com>
2019-11-20 23:48:28 +0900

    edit mainpage

--- 2B/static/mainpage.js
+++ 2B/static/mainpage.js
@@ -1,7 +1,8 @@
+window.onload = () => {
+    var mapContainer = document.getElementById('maps'), mapOption = {
+        center : new kakao.maps.LatLng(35.228002, 128.681816),
+        level : 3
+    };
 
-var mapContainer = document.getElementById('maps'), mapOption = {
-    center : new kakao.maps.LatLng(35.228002, 128.681816),
-    level : 3
-};
-
-var map = new kakao.maps.Map(mapContainer, mapOption);
+    var map = new kakao.maps.Map(mapContainer, mapOption);
+}
