833e2fd8bd832719176adfcfe3d6e1e0d0f371f5
TayPark <31007427+TayPark@users.noreply.github.com>
2019-11-21 23:17:51 +0900

    Create gps.js

---
+++ 2B/static/_js/gps.js
@@ -0,0 +1,18 @@
+////
+// GPS
+////
+
+let myLat, myLong; // User's Latitude and Longtitude 
+let gps = document.getElementById('gps');
+
+gps.addEventListener('click', function(){
+    navigator.geolocation.getCurrentPosition(function(position) {
+        if(navigator.geolocation){
+            //show_loc(position.coords.latitude, position.coords.longitude);
+            myLatitude = position.coords.latitude;
+            myLongtitude = position.coords.latitude;
+        } else {
+            alert('GPS를 사용할 수 없습니다.')
+        }
+      });
+});
