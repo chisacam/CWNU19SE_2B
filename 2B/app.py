5b559a473c8dc13cd337a370905c6b16fb98c2d2
chisacam <dladsds123@gmail.com>
2019-11-25 02:54:31 +0900

    쿠키관련 예외처리및 최근기록, 북마크 기능 복구

--- 2B/app.py
+++ 2B/app.py
@@ -7,7 +7,8 @@ from flask import make_response
 import requests
 import urllib3.request
 
-def weatherinfo():
+
+def weatherInfo():
     base_address = "https://api.openweathermap.org/data/2.5/weather?id=1846326&appid=7a60cf8ebe413584303acc4e2bf4cffe"
     req_weather = requests.get(base_address)
     base_info = req_weather.text
@@ -31,11 +32,29 @@ app = Flask(__name__)
 IDkey = "jpfybhk69d"
 SecretKey = "RuIMY0ILxMIf6ZZCyA9BIb2syBOXqnJrVEYzP5GX"
 
+
 @app.route('/')
 @app.route('/main')
 def main_Page():
-    main_weather = weatherinfo()
-    return render_template("index.html", weather=main_weather["weather"])
+    main_weather = weatherInfo()
+    recentList = request.cookies.get('recentlist')
+    if recentList is None:
+        resp = make_response()
+        defaultCookie = {
+            "depart": [
+
+            ],
+            "dest": [
+
+            ]
+        }
+        defaultJson = json.dumps(defaultCookie)
+        resp = make_response(render_template("index.html", weather=main_weather["weather"]))
+        resp.set_cookie('recentlist', defaultJson)
+        resp.set_cookie('booklist', defaultJson)
+        return resp
+    else:
+        return render_template("index.html", weather=main_weather["weather"])
 
 
 @app.route('/js')
@@ -51,31 +70,33 @@ def jse():
 
 @app.route('/weather')
 def Weather_page():
-    weather = weatherinfo()
-    #print(weather)
+    weather = weatherInfo()
+    # print(weather)
     return render_template("weather.html", weather=weather["weather"], temp=weather["temp"])
 
 
 @app.route('/searchRecent', methods=['POST'])
 def recent_search():
-    print(request.form['selector'])
+    # print(request.form['selector'])
     sel = request.form['selector']
-#    x = request.form['x']
-#    y = request.form['y']
-#    print(sel, name, x, y)
+    recentList = eval(request.cookies.get('recentlist'))
+    if sel in 'depart':  # 출발지
+        return render_template("search_recent.html", recentDepList=recentList["depart"])
 
-    #if sel in 'depart': #출발지
-
-    #if sel in 'dest': #목적
-
-    #recentlist = eval(request.cookies.get('recent'))
-    return render_template("search_recent.html")
+    if sel in 'dest':  # 목적
+        return render_template("search_recent.html", recentDesList=recentList["dest"])
 
 
-@app.route('/searchBookmark')
+@app.route('/searchBookmark', methods=['POST'])
 def recent_bookmark():
-    #booklist = eval(request.cookies.get('bookmark'))
-    return render_template("search_bookmark.html")
+    # bookmark 페이지 넘어갈때도 sel 변수 전달 필요
+    sel = request.form['selector']
+    bookList = eval(request.cookies.get('booklist'))
+    if sel in 'depart':  # 출발지
+        return render_template("search_bookmark.html", recentDepList=bookList["depart"])
+
+    if sel in 'dest':  # 목적
+        return render_template("search_bookmark.html", recentDesList=bookList["dest"])
 
 
 @app.route('/busterminalSelect')
@@ -90,17 +111,18 @@ def nubijaselect():
 
 @app.route('/searchText')
 def search_text():
-    name = "창원시청"#request.form['name']
-#    print(request.form['selector'])
-#    sel = request.form['selector']
-    x = str(127.1054328)#request.form['x']
-    y = str(37.3595963)#request.form['y']
-#    print(sel, name, x, y)
-    params={'query': name, "coordinate": x + "," + y}
-    headers = {"X-NCP-APIGW-API-KEY-ID":IDkey, "X-NCP-APIGW-API-KEY":SecretKey}
+    name = "창원시청"  # request.form['name']
+    #    print(request.form['selector'])
+    #    sel = request.form['selector']
+    x = str(127.1054328)  # request.form['x']
+    y = str(37.3595963)  # request.form['y']
+    #    print(sel, name, x, y)
+    params = {'query': name, "coordinate": x + "," + y}
+    headers = {"X-NCP-APIGW-API-KEY-ID": IDkey, "X-NCP-APIGW-API-KEY": SecretKey}
     base_search_addr = "https://naveropenapi.apigw.ntruss.com/map-place/v1/search"
     res = requests.get(base_search_addr, params=params, headers=headers)
     code = res.status_code
+
     if code == 200:
         result = res.json()["places"]
         print(result)
