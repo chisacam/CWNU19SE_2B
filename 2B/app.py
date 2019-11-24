2156611c3f74c803e2c7982df87b3f3dfc8b9548
chisacam <dladsds123@gmail.com>
2019-11-24 19:34:19 +0900

    back add

--- 2B/app.py
+++ 2B/app.py
@@ -1,64 +1,71 @@
+import json
+import requests
+
 from flask import Flask
 from flask import render_template
-from flask import jsonify
-import json
+from flask import request
+from flask import make_response
 
 app = Flask(__name__)
 
 
 @app.route('/')
-@app.route('/main')
-def main_page():
+def mainPage():
     return render_template("index.html")
 
-@app.route('/js')
-def jse():
-    geoInfo = open('static/terminalInfo.json', 'r')
-    nameInfo = open('static/terminalName.json', 'r')
-    nameInfo = json.loads(nameInfo.read())
-    nameInfo = json.dumps(nameInfo, ensure_ascii=False)
-    geoInfo = json.loads(geoInfo.read())
-    geoInfo = json.dumps(geoInfo, ensure_ascii=False)
-    return jsonify(namedata=nameInfo)
 
 @app.route('/weather')
-def weather_page():
-    return render_template("weather.html")
+def getWeatherInfo():
+    base_address = "https://api.openweathermap.org/data/2.5/weather?id=1846326&appid=7a60cf8ebe413584303acc4e2bf4cffe"
+    req_weather = requests.get(base_address)
+    base_info = req_weather.text
+    result = json.loads(base_info)
+    weather = result["weather"][0]["main"]
+    temp = result["main"]["temp"] - 273
+    icon = "http://openweathermap.org/img/w/" + result["weather"][0]["icon"] + ".png"
+    """print(round(temp), weather)"""
+    return render_template("weather.html", weather=weather, temp=round(temp), icon=icon)
 
 
-@app.route('/searchRecent')
-def recent_search():
-    return render_template("search_recent.html")
+@app.route('/bookmark')
+def loadbookmarkList():
+    booklist = eval(request.cookies.get('bookmark'))
+    return booklist
 
 
-@app.route('/searchBookmark')
-def recent_bookmark():
-    return render_template("search_bookmark.html")
+@app.route('/recent')
+def loadrecentList():
+    recentlist = eval(request.cookies.get('recent'))
+    return recentlist
 
 
-@app.route('/busterminalSelect')
-def busTerminalSelect():
-    return render_template('bus_terminal_select.html')
+@app.route('/search')
+def test():
+    """
+    bookmark = {
+        "경남도청": {
+            "X": "111.222",
+            "Y": "222.333"
+        },
+        "창원시청": {
+            "X": "111.111",
+            "Y": "222.222"
+        }
+    }"""
+
+    inputstr = "경기도청"
+    inputstr2 = "창원시청"
+    bookmark = eval(request.cookies.get('bookmark'))
+    cookie = json.dumps(bookmark, ensure_ascii=False, indent=4)
+    test = json.loads(cookie)
+    print(type(cookie))
+    print(test["경남도청"])
+#    resp = make_response("Cookie Setting Complete")
+#    resp.set_cookie('bookmark', cookie)
+    return print(cookie)
 
 
-@app.route('/nubijaSelect')
-def nubijaselect():
-    return render_template('Nubija_terminal_select.html')
-
-
-@app.route('/searchText')
-def search_text():
-    return render_template("search_text.html")
-
-
-@app.route('/naviNubija')
-def navi_nibija():
-    return render_template("navigation_nubija.html")
-
-@app.route('/naviBus')
-def navi_bus():
-    return render_template('navigation_bus.html')
-
+"""@app.route('/result')"""
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug = True)
+    app.run(host='0.0.0.0')
