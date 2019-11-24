cdaf6cae391f0e9e2b3fb3e5052bc12436adbfc6
chisacam <dladsds123@gmail.com>
2019-11-25 00:20:00 +0900

    네이버 API 호출기능 추가

--- 2B/app.py
+++ 2B/app.py
@@ -5,6 +5,7 @@ import json
 from flask import request
 from flask import make_response
 import requests
+import urllib3.request
 
 def weatherinfo():
     base_address = "https://api.openweathermap.org/data/2.5/weather?id=1846326&appid=7a60cf8ebe413584303acc4e2bf4cffe"
@@ -27,7 +28,8 @@ def weatherinfo():
 
 
 app = Flask(__name__)
-
+IDkey = "jpfybhk69d"
+SecretKey = "RuIMY0ILxMIf6ZZCyA9BIb2syBOXqnJrVEYzP5GX"
 
 @app.route('/')
 @app.route('/main')
@@ -57,6 +59,15 @@ def Weather_page():
 @app.route('/searchRecent', methods=['POST'])
 def recent_search():
     print(request.form['selector'])
+    sel = request.form['selector']
+#    x = request.form['x']
+#    y = request.form['y']
+#    print(sel, name, x, y)
+
+    #if sel in 'depart': #출발지
+
+    #if sel in 'dest': #목적
+
     #recentlist = eval(request.cookies.get('recent'))
     return render_template("search_recent.html")
 
@@ -79,11 +90,37 @@ def nubijaselect():
 
 @app.route('/searchText')
 def search_text():
-    return render_template("search_text.html")
+    name = "창원시청"#request.form['name']
+#    print(request.form['selector'])
+#    sel = request.form['selector']
+    x = str(127.1054328)#request.form['x']
+    y = str(37.3595963)#request.form['y']
+#    print(sel, name, x, y)
+    params={'query': name, "coordinate": x + "," + y}
+    headers = {"X-NCP-APIGW-API-KEY-ID":IDkey, "X-NCP-APIGW-API-KEY":SecretKey}
+    base_search_addr = "https://naveropenapi.apigw.ntruss.com/map-place/v1/search"
+    res = requests.get(base_search_addr, params=params, headers=headers)
+    code = res.status_code
+    if code == 200:
+        result = res.json()["places"]
+        print(result)
+        return render_template("search_text.html", result=result)
+    else:
+        print(code)
 
 
 @app.route('/naviNubija')
 def navi_nibija():
+    x1 = str(127.1)
+    y1 = str(37.3)
+    x2 = str(127.1054328)  # request.form['x']
+    y2 = str(37.3595963)  # request.form['y']
+    params = {'start': x1 + "," + y1, "goal": x2 + "," + y2}
+    headers = {"X-NCP-APIGW-API-KEY-ID": IDkey, "X-NCP-APIGW-API-KEY": SecretKey}
+    base_search_addr = "https://naveropenapi.apigw.ntruss.com/map-direction/v1/driving"
+    res = requests.get(base_search_addr, params=params, headers=headers)
+    code = res.status_code
+    print(res.json()["route"]["traoptimal"][0]["guide"])
     return render_template("navigation_nubija.html")
 
 
