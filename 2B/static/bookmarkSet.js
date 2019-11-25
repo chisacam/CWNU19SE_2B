526aa83b2d83576e3bb49a10bf115b180137140f
SiGae <SiGae@protonmail.com>
2019-11-25 21:53:33 +0900

    add starbutton change

--- 2B/static/bookmarkSet.js
+++ 2B/static/bookmarkSet.js
@@ -1,5 +1,8 @@
-function bmStateSet(currentBM) {
+function bmStateSet(event) {
   // 별, 안 별
+    alert('asdf');
+    document.getElementById('chch').value = "true";
+    event.src = "/static/icon/star_empty.svg"
 }
 
 function setLocationAsBM() {
