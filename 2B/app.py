756873ec7969c55c19830ded798dbd4c1c9fdf9f
chisacam <dladsds123@gmail.com>
2019-11-25 20:09:45 +0900

    북마크,최근기록 출발목적지 변수 전달수정

--- 2B/app.py
+++ 2B/app.py
@@ -12,20 +12,20 @@ def weatherInfo():
     base_address = "https://api.openweathermap.org/data/2.5/weather?id=1846326&appid=7a60cf8ebe413584303acc4e2bf4cffe"
     req_weather = requests.get(base_address)
     base_info = req_weather.text
-    result = json.loads(base_info)
-    weather = result["weather"][0]["main"]
-    temp = result["main"]["temp"] - 273
-    icon = result["weather"][0]["icon"]
+    weatherResult = json.loads(base_info)
+    weather = weatherResult["weather"][0]["main"]
+    temp = weatherResult["main"]["temp"] - 273
+    icon = weatherResult["weather"][0]["icon"]
     if icon in "50d":
         weather = '/static/icon/weather/mist.svg'
     else:
         weather = weather.lower()
         weather = '/static/icon/weather/{}.svg'.format(weather)
-    weatherdict = {
+    weatherDict = {
         "weather": weather,
         "temp": round(temp)
     }
-    return weatherdict
+    return weatherDict
 
 
 app = Flask(__name__)
@@ -34,27 +34,59 @@ SecretKey = "RuIMY0ILxMIf6ZZCyA9BIb2syBOXqnJrVEYzP5GX"
 
 
 @app.route('/')
-@app.route('/main')
 def main_Page():
     main_weather = weatherInfo()
     recentList = request.cookies.get('recentlist')
     if recentList is None:
-        resp = make_response()
         defaultCookie = {
             "depart": [
-
+                """
+                "창원시청":{
+                    "X":"123.123",
+                    "Y":"345.345",
+                    "isBook":"true"
+                }, # 최근기록 리스트
+                "창원대학교":{
+                    "X":"342.423",
+                    "Y":"123.523"
+                } # 북마크 리스트
+                """
             ],
             "dest": [
-
+                # 위와 동일
             ]
+        }
+        defaultRoute = {
+            "depart": {
+                """
+                "창원시청":{
+                    "X":"123.123",
+                    "Y":"345.345"
+                }
+                """
+            },
+            "dest": {
+                """
+                "창원대학교":{
+                    "X": "342.423",
+                    "Y": "123.523"
+                }
+                """
+            }
         }
         defaultJson = json.dumps(defaultCookie)
         resp = make_response(render_template("index.html", weather=main_weather["weather"]))
         resp.set_cookie('recentlist', defaultJson)
         resp.set_cookie('booklist', defaultJson)
+        resp.set_cookie('routeinfo', defaultRoute)
         return resp
     else:
         return render_template("index.html", weather=main_weather["weather"])
+
+
+@app.route('/main')
+def result_Page():
+    return render_template("index.html")
 
 
 @app.route('/js')
@@ -77,26 +109,27 @@ def Weather_page():
 
 @app.route('/searchRecent', methods=['POST'])
 def recent_search():
-    # print(request.form['selector'])
-    sel = request.form['selector']
+    sel = request.form['sel']
+    print(sel)
     recentList = eval(request.cookies.get('recentlist'))
     if sel in 'depart':  # 출발지
-        return render_template("search_recent.html", recentDepList=recentList["depart"])
+        return render_template("search_recent.html", recentDepList=recentList["depart"], sel=sel)
 
-    if sel in 'dest':  # 목적
-        return render_template("search_recent.html", recentDesList=recentList["dest"])
+    if sel in 'dest':  # 목적지
+        return render_template("search_recent.html", recentDesList=recentList["dest"], sel=sel)
 
 
 @app.route('/searchBookmark', methods=['POST'])
 def recent_bookmark():
     # bookmark 페이지 넘어갈때도 sel 변수 전달 필요
-    sel = request.form['selector']
+    sel = request.form['sel']
+    print(sel)
     bookList = eval(request.cookies.get('booklist'))
     if sel in 'depart':  # 출발지
-        return render_template("search_bookmark.html", recentDepList=bookList["depart"])
+        return render_template("search_bookmark.html", recentDepList=bookList["depart"], sel=sel)
 
-    if sel in 'dest':  # 목적
-        return render_template("search_bookmark.html", recentDesList=bookList["dest"])
+    if sel in 'dest':  # 목적지
+        return render_template("search_bookmark.html", recentDesList=bookList["dest"], sel=sel)
 
 
 @app.route('/busterminalSelect')
@@ -105,18 +138,15 @@ def busTerminalSelect():
 
 
 @app.route('/nubijaSelect')
-def nubijaselect():
+def nubijaTerminalSelect():
     return render_template('Nubija_terminal_select.html')
 
 
 @app.route('/searchText')
 def search_text():
-    name = "창원시청"  # request.form['name']
-    #    print(request.form['selector'])
-    #    sel = request.form['selector']
-    x = str(127.1054328)  # request.form['x']
-    y = str(37.3595963)  # request.form['y']
-    #    print(sel, name, x, y)
+    name = "누비자"  # request.form['name']
+    x = str(128.6818020)  # request.form['x']
+    y = str(35.2279269)  # request.form['y']
     params = {'query': name, "coordinate": x + "," + y}
     headers = {"X-NCP-APIGW-API-KEY-ID": IDkey, "X-NCP-APIGW-API-KEY": SecretKey}
     base_search_addr = "https://naveropenapi.apigw.ntruss.com/map-place/v1/search"
@@ -126,7 +156,15 @@ def search_text():
     if code == 200:
         result = res.json()["places"]
         print(result)
+        """
+        if sel in 'depart':  # 출발지
+            return render_template("search_text.html", result=result, sel=sel)
+
+        if sel in 'dest':  # 목적지
+            return render_template("search_text.html", result=result, sel=sel)
+        """
         return render_template("search_text.html", result=result)
+
     else:
         print(code)
 
