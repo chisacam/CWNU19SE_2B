0fb67769b5430ebd0748d9e6cfdc67df6acc960b
sigae <SiGae@protonmail.com>
2019-11-27 17:21:28 +0900

    Add

--- 2B/static/bookmarkSet.js
+++ 2B/static/bookmarkSet.js
@@ -3,13 +3,22 @@
       - 북마크는 별 아이콘이 노란색, 최근기록은 빈 별이다. 이는 검색기록에서 변수 1개로 제어한다.
     2. 검색기록에서 최근기록의 별을 클릭하면 별 아이콘이 노란색으로 바뀌고 다른 페이지(검색을 수행하거나 북마크 목록)으로 이동하면 추가되어야한다.
       - 북마크의 순서는 검색기록과 같이 최근 등록한 북마크가 상단에 위치한다.
-    3. 검색기록에서 북마크로 등록할 때에는 넘겨주었던 인덱스를 참고하여 해당 데이터를 북마크에도 복사하여 저장한다.5607]
+    3. 검색기록에서 북마크로 등록할 때에는 넘겨주었던 인덱스를 참고하여 해당 데이터를 북마크에도 복사하여 저장한다.
 */
 
 function bmStateSet(event) {
-  // 별, 안 별
-    document.getElementById('chch').value = "true";
-    event.src = "/static/icon/star_empty.svg"
+  // 최근 검색에서 북마크를 누르면 
+  // document.getElementById('chch').value = "true";
+  // event.src = "/static/icon/star_empty.svg"
+
+  var className = event.className;
+
+  if(document.getElementsByClassName(className).src == "/static/icon/star_empty.svg") {
+    document.getElementsByClassName(className).src = "/static/icon/gold_star.svg";
+  }
+  else {
+    document.getElementsByClassName(className).src = "/static/icon/star_empty.svg";
+  }
 }
 
 function setLocationAsBM(event) {
@@ -25,5 +34,12 @@ function setLocationAsR(event) {
         document.getElementById('selname').value = temp[2];
         document.getElementById('selX').value = temp[3];
         document.getElementById('selY').value = temp[4];
-
 }
+
+// 웹페이지를 벗어날 때 별표시를 한 것 체크 
+window.addEventListener("beforeunload", () => {
+    // isBook == false, star == gold_star 인 것을 골라내서 북마크에 추가
+  for(var i=0;i<5;i++){
+
+  }
+});
