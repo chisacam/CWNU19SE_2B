71e40d9588b4737d85e3cb426c102d5f6623f479
SiGae <SiGae@protonmail.com>
2019-11-27 12:03:17 +0900

    error fix

--- 2B/static/bookmarkSet.js
+++ 2B/static/bookmarkSet.js
@@ -6,10 +6,9 @@ function bmStateSet(event) {
 
 function setLocationAsBM(event) {
     var temp = document.getElementsByClassName(event.className);
-        document.getElementById('selname').value = temp[0];
-        document.getElementById('selX').value = temp[1];
-        document.getElementById('selY').value = temp[2];
-
+        document.getElementById('selectedName').value = temp[0].value;
+        document.getElementById('selectedX').value = temp[1].value;
+        document.getElementById('selectedY').value = temp[2].value;
 }
 
 
