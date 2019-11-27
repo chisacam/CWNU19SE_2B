2fe09d0fef83d5f9e5e3b7f1ff6650938188d682
chisacam <dladsds123@gmail.com>
2019-11-27 22:14:57 +0900

    name with sel

--- 2B/app.py
+++ 2B/app.py
@@ -184,7 +184,7 @@ def result_Page():
                             }
                         })
         saveRoute = json.dumps(startEndCheck, ensure_ascii=False)
-        resp = make_response(render_template("index.html", weather=main_weather["weather"], name=name))
+        resp = make_response(render_template("index.html", weather=main_weather["weather"], name=name, sel=sel))
         resp.set_cookie('routeinfo', saveRoute)
         return resp
     else:
@@ -216,15 +216,15 @@ def recent_search():
     else:
         isServiceTime = True
     sel = request.form['sel']
-    """hiddenuserLat = request.form['hiddenuserLat']
-    hiddenuserLong = request.form['hiddenuserLong']"""
+    hiddenuserLat = request.form['hiddenuserLat']
+    hiddenuserLong = request.form['hiddenuserLong']
     recentList = eval(request.cookies.get('recentlist'))
     bookList = eval(request.cookies.get('booklist'))
     recentDepartLen = len(recentList["depart"])
     recentDestLen = len(recentList["dest"])
     bookDepartLen = len(bookList["depart"])
     bookDestLen = len(bookList["dest"])
-
+    print(hiddenuserLat, hiddenuserLong)
     if bookDepartLen != 0 and bookDestLen != 0:
         for checkRecent in range(0, recentDepartLen):
             for key, value in recentList["depart"][checkRecent].items():
